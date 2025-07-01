1373ebcc1ed0d91e85ffa649476798801df9d5b9
tariful6 <tariful2306@gmail.com>
2025-07-01 18:23:20 +0600

    data sort filter implement

--- src/Pages/AllJobs/AllJobs.jsx
+++ src/Pages/AllJobs/AllJobs.jsx
@@ -6,6 +6,10 @@ const AllJobs = () => {
     
     const [itemPerPage, setItemPerPage] = useState(4);
     const [currentPage, setCurrentPage] = useState(1);
+    const [filter, setFilter] = useState('')
+    const [sort, setSort] = useState('')
+    const [search, setSearch] = useState('')
+    const [searchText, setSearchText] = useState('')
 
     const [jobs, setJobs] = useState([]);
     const [count, setCount] = useState(0);
@@ -20,20 +24,20 @@ const AllJobs = () => {
         // },[])
         
         
-        
         useEffect(()=>{
-            axios(`http://localhost:5000/allJobs?page=${currentPage}&size=${itemPerPage}`)
+            axios(`http://localhost:5000/allJobs?page=${currentPage}&size=${itemPerPage}&filter=${filter}&sort=${sort}&search=${search}`)
             .then(data => {
                 setJobs(data.data)
             })
-        },[currentPage, itemPerPage])
+        },[currentPage, itemPerPage, filter, sort, search])
+
         
         useEffect(()=>{
-            axios('http://localhost:5000/jobsCount')
+            axios(`http://localhost:5000/jobsCount?filter=${filter}&search=${search}`)
             .then(data => {
               setCount(data.data.count);
             })
-        },[])
+        },[filter,search])
     
     const numberOfPages = Math.ceil(count / itemPerPage)
         
@@ -45,32 +49,55 @@ const AllJobs = () => {
         setCurrentPage(value)
         
     }
+
+    const handleReset = ()=>{
+        setFilter('')
+        setSort('')
+        setSearch('')
+        setSearchText('')
+    }
+
+    const handleSearch = (e) =>{
+        e.preventDefault()
+        setSearch(searchText)
+    }
+
+    
     return (
         <div className='container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between'>
         <div>
             <div className='flex flex-col md:flex-row justify-center items-center gap-5 '>
             <div>
                 <select
+                onChange={e => {
+                    setFilter(e.target.value);
+                    setCurrentPage(1)
+                }}
+                value={filter}
                 name='category'
                 id='category'
                 className='border p-4 rounded-lg'
                 >
-                <option value=''>Filter By Category</option>
+                <option value='' disabled>Filter By Category</option>
                 <option value='Web Development'>Web Development</option>
                 <option value='Graphics Design'>Graphics Design</option>
                 <option value='Digital Marketing'>Digital Marketing</option>
                 </select>
             </div>
 
-            <form>
+
+            <form onSubmit={handleSearch}>
                 <div className='flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
                 <input
+                   onChange={e => setSearchText(e.target.value)}
+                   value={searchText}
                     className='px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent'
                     type='text'
                     name='search'
                     placeholder='Enter Job Title'
                     aria-label='Enter Job Title'
                 />
+
 
                 <button className='px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none'>
                     Search
@@ -79,8 +106,13 @@ const AllJobs = () => {
             </form>
             <div>
                 <select
-                name='category'
-                id='category'
+                onChange={e => {
+                    setSort(e.target.value);
+                    setCurrentPage(1)
+                }}
+                value={sort}
+                name='sort'
+                id='sort'
                 className='border p-4 rounded-md'
                 >
                 <option value=''>Sort By Deadline</option>
@@ -88,7 +120,7 @@ const AllJobs = () => {
                 <option value='asc'>Ascending Order</option>
                 </select>
             </div>
-            <button className='btn'>Reset</button>
+            <button onClick={handleReset} className='btn'>Reset</button>
             </div>
             <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
             {jobs.map(job => (
